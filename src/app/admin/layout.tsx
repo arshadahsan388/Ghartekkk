480e9c0094766adcf46abc5e22ac488bf66baddd
Raf <raf451810@gmail.com>
2025-08-28 09:17:08 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -74,16 +74,12 @@ export default function AdminLayout({
   }
 
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>
-        <div className="flex min-h-screen w-full flex-col bg-muted/40">
-          <AdminSidebar />
-          <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
-            {children}
-          </div>
-          <Toaster />
-        </div>
-      </body>
-    </html>
+    <div className={`flex min-h-screen w-full flex-col bg-muted/40 ${inter.className} antialiased`}>
+      <AdminSidebar />
+      <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+        {children}
+      </div>
+      <Toaster />
+    </div>
   );
 }
