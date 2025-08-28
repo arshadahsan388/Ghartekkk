ae1748427398a2208e3cf0c3f3242e1eb2e14777
Raf <raf451810@gmail.com>
2025-08-28 09:08:47 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/admin/layout/AdminSidebar.tsx
+++ src/components/admin/layout/AdminSidebar.tsx
@@ -28,6 +28,7 @@ import { useEffect, useState } from 'react';
 import AdminNav from './AdminNav';
 import Logo from '@/components/icons/Logo';
 import { ThemeSwitcher } from '@/components/theme/ThemeSwitcher';
+import { TooltipProvider } from '@/components/ui/tooltip';
 
 const mainNavItems = [
     { href: "/admin", label: "Dashboard", icon: Home },
@@ -70,20 +71,22 @@ export function AdminSidebar() {
   return (
     <>
       <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
-        <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
-            <Link
-                href="/"
-                className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
-            >
-                <Logo className="h-5 w-5 transition-all group-hover:scale-110" />
-                <span className="sr-only">Pak Delivers</span>
-            </Link>
-            <AdminNav navItems={mainNavItems} />
-        </nav>
-        <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
-            <ThemeSwitcher />
-             <AdminNav navItems={[settingsNavItem]} />
-        </nav>
+        <TooltipProvider>
+            <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
+                <Link
+                    href="/"
+                    className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+                >
+                    <Logo className="h-5 w-5 transition-all group-hover:scale-110" />
+                    <span className="sr-only">Pak Delivers</span>
+                </Link>
+                <AdminNav navItems={mainNavItems} />
+            </nav>
+            <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
+                <ThemeSwitcher />
+                <AdminNav navItems={[settingsNavItem]} />
+            </nav>
+        </TooltipProvider>
       </aside>
 
       <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
